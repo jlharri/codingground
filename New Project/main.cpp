8822187986f99b5fec937dba98e62c135028c1c5
jlharri <jlharri@jerryharri.com>
2015-01-31 10:39:52 -0500

    Update main.cpp

--- New Project/main.cpp
+++ New Project/main.cpp
@@ -15,19 +15,28 @@ void print_numbers(const char* text, double a1, int a2)
 
 int main()
 {
-   double answer1 = 1.8;
+   double answer1;
    int answer2;
 
-   answer2 = ++answer1;
-   print_numbers("++answer1", answer1, answer2);
-   
-   answer1 = 1.8;
+   answer1 = 1.8; 
    answer2 = answer1++;
    print_numbers("answer1++", answer1, answer2);
 
    answer1 = 1.8;
+   answer2 = ++answer1;
+   print_numbers("++answer1", answer1, answer2);
+   
+   answer1 = 1.8;
    answer2 = round(answer1);
    print_numbers("round(answer1)", answer1, answer2);
+
+   answer1 = 1.3;
+   answer2 = round(answer1);
+   print_numbers("round(answer1)", answer1, answer2);
+
+   answer1 = 1.3;
+   answer2 = ceil(answer1);
+   print_numbers("ceil(answer1)", answer1, answer2);
 
    return 0;
 }
