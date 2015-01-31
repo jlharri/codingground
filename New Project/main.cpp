73d5798d9c17ba00a4c736321e81e415e399a14b
jlharri <jlharri@jerryharri.com>
2015-01-31 10:30:53 -0500

    Update main.cpp

--- New Project/main.cpp
+++ New Project/main.cpp
@@ -4,20 +4,27 @@
 
 using namespace std;
 
+void print_numbers(double a1, int a2)
+{
+   cout << "Answer1 = " << a1 << endl;
+   cout << "Answer2 = " << a2 << endl;    
+
+   cout << endl;
+   return;    
+}
+
 int main()
 {
    double answer1 = 1.8;
-   int answer2, answer3;
-   answer2 = ++answer1;
-   cout << "Answer1 = " << answer1 << endl;
-   cout << "Answer2 = " << answer2 << endl;    
+   int answer2;
 
-   cout << endl;
+   answer2 = ++answer1;
+   print_numbers(answer1, answer2);
    
    answer1 = 1.8;
-   answer3 = answer1++;
-   cout << "Answer1 = " << answer1 << endl;
-   cout << "Answer3 = " << answer3 << endl;
+   answer2 = answer1++;
+   print_numbers(answer1, answer2);
+
    return 0;
 }
 
