a8888b624464b6a97696584bd3f03521f9f9a811
Jerry Harris <>
2023-03-05 14:22:35 -0500

    Added new file and new line in some file. - JLH

---
+++ sample-widget.js
@@ -0,0 +1,9 @@
+$(() => {
+    $('#gridContainer').dxDataGrid({
+      dataSource: 'data/customers.json',
+      columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
+      showBorders: true,
+    });
+  });
+
+  
